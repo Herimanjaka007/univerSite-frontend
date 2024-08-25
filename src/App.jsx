07d17e387e5f527f5c890bc39448314f4daa3d3b
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2024-08-25 14:38:12 +0300

    Basic App routes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+const App = () => {
+  <BrowserRouter>
+    <Routes>
+
+    </Routes>
+  </BrowserRouter>
+}
+
+export default App;
