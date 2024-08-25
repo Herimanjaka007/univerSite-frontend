c1a2cdff744d46b4ae4baa2ec2714b8d501d1f18
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2024-08-25 15:16:54 +0300

    Add University Card Components

---
+++ src/components/UniversityCard/index.jsx
@@ -0,0 +1,31 @@
+const UniversityCard = ({ img, nom, adress }) => {
+    return (
+
+        <div className="col-lg-3 col-md-6 mt-3">
+            <div className="card">
+                <img
+                    src={img}
+                    className="card-img-top"
+                    alt="..."
+                />
+                <div className="card-body">
+                    <h5 className="card-title text-center">
+                        {nom}
+                    </h5>
+                </div>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <p className="text-muted">
+                            {" "}
+                            <i className="fas fa-location-dot fs-4 mx-2 mt-2" />{" "}
+                            {adress}
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    );
+}
+
+export default UniversityCard;
