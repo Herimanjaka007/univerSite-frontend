320d367b004d919c444019b45c0ceb7ce247ea49
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2024-08-25 15:41:08 +0300

    Create Alert Components

---
+++ src/components/Alert/index.jsx
@@ -0,0 +1,10 @@
+const Alert = ({ message, handler }) => {
+    return (
+        <div className="alert alert-danger d-flex">
+            <p>{message}</p>
+            <span className="btn text-secondary ms-auto" onClick={handler}>X</span>
+        </div>
+    );
+}
+
+export default Alert;
