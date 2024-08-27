2740b0ab87cc3a4dab60a0cde20c796d2e9a6a32
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2024-08-27 08:55:16 +0300

    fix conflict

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -67,14 +67,11 @@ const Login = () => {
                         <input type="text" name="username" id="userName" placeholder="Nom d'utilisateur" />
                     </div>
                     <div className="form-field d-flex align-items-center">
-<<<<<<< HEAD
                         <span className="fas fa-key" />
                         <input type={!pwdVisible ? "password" : "text"} name="password" id="pwd" placeholder="Mot de passe" />
                         <span className="show" onClick={() => setPwdVisible(!pwdVisible)}><i className={pwdVisible ? "bi bi-eye-slash-fill" : "bi bi-eye-fill"}></i></span>
-=======
                         <span className="fas fa-key ms-2" />
                         <input type="password" name="password" id="pwd" placeholder="Mot de passe" />
->>>>>>> 2ba5e9dc5747629006f6169ac8c668bc92401e61
                     </div>
                     {error && <Alert message={error} handler={() => setError(null)} />}
                     <button className="btn__login mt-3">
